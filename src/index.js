83b92560ab5d3d42518ad2cda86fc27495ba2569
Joby Joseph <jobyjose@petco.com>
2021-04-19 18:57:07 +0530

    test

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('rootdcdfefe')
+  document.getElementById('root-demo')
 );
 
 // If you want to start measuring performance in your app, pass a function
